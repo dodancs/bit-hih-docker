e0175d5935170aaca893340d65bbcf015a84e734
Dominik Dancs <dodancs@moow.info>
2020-10-07 23:04:54 +0200

    Another argument parsing fix, added help text

--- server.py
+++ server.py
@@ -7,30 +7,39 @@ import uuid
 import logging
 import logging.handlers
 import docker
+import ipaddress
 
 #####################
 #      Logging      #
 #####################
 
 logger = logging.getLogger('canary-server')
-handler = logging.handlers.SysLogHandler(address='/dev/log')
-handler.setFormatter(logging.Formatter(
+logging_format = logging.Formatter(
     '%(name)s: [%(levelname)s] %(message)s'
-))
-logger.addHandler(handler)
+)
+if os.name != 'nt':
+    handler_linux = logging.handlers.SysLogHandler(address='/dev/log')
+    handler_linux.setFormatter(logging_format)
+    logger.addHandler(handler_linux)
 logger.setLevel(logging.INFO)
 
 #####################
 #       Helpers     #
 #####################
 
+_VERSION = '1.0.0'
 _DEBUG = False
+_CONFIG_FILE = 'config.json'
 _CONFIG = None
+_CONFIG_OVERRIDE_BIND = None
+_CONFIG_OVERRIDE_CONNS = None
 _DOCKER_CLIENT = None
 _DOCKER_IMAGES = []
+_PULL = True
 
 
 def info(message):
+    cprint('[INFO]: %s' % message, 'white')
     logging.info(message)
 
 
@@ -47,10 +56,22 @@ def error(message):
 
 def printHelp():
     cprint(
-        'Please run sync.py with a parameter:\n~$ python3 ./sync.py {parameter}\n\
-            \t-s\tSetup basic server environment\n\
-            \t-d\tRun in daemon mode - automatically check and sync canaries', 'cyan')
+        'python3 ./server.py [OPTIONS]\n\
+    -h --help                   Show help\n\
+    -v --version                Show program version\n\
+    -d --debug                  Show debug messages\n\
+       --bind [ADDR]            Override IP address bind\n\
+       --max-connections [N]    Override maximum number of connections to the server\n\
+    -c --config [FILE]          Use custom config file location\n\
+       --dont-pull              Don\'t pull images from Docker hub\
+    ', 'cyan')
     exit(1)
+
+
+def printVersion():
+    global _VERSION
+    cprint('HIH-Docker version {}'.format(_VERSION), 'cyan')
+    exit(0)
 
 #####################
 #   Config loader   #
@@ -58,18 +79,16 @@ def printHelp():
 
 
 def loadConfig():
-    global _CONFIG
+    global _CONFIG, _CONFIG_FILE
 
     info('Loading server configuration...')
 
-    _CONFIG_FILENAME = 'config.json'
     _CONFIG_SCHEMA = json.loads(open('config.schema.json').read())
 
     try:
-        _CONFIG = json.loads(open(_CONFIG_FILENAME).read())
+        _CONFIG = json.loads(open(_CONFIG_FILE).read())
     except Exception as e:
-        error('Cannot open configuration file! Looking for {}'.format(
-            os.path.join(os.getcwd(), _CONFIG_FILENAME)))
+        error('Cannot open configuration file!')
         logging.error(e)
         exit(1)
 
@@ -102,22 +121,63 @@ def init():
     debug(_DOCKER_IMAGES)
 
     # pull docker images
+    if _PULL:
+        info('Updating Docker images...')
+        for image in _DOCKER_IMAGES:
+            info('Pulling {}...'.format(image))
+            _DOCKER_CLIENT.images.pull(image)
 
 
 ########################
 #   Argument parsing   #
 ########################
 
-sys.argv.pop()
-for arg in sys.argv:
+sys.argv.pop(0)
+while len(sys.argv) > 0:
+    arg = sys.argv.pop(0)
 
-    if '--help' in sys.argv or '-h' in sys.argv:
+    if arg in ['--help', '-h']:
         printHelp()
 
+    elif arg in ['--version', '-v']:
+        printVersion()
+
     # enable debugging
-    elif '--debug' in sys.argv or '-D' in sys.argv:
+    elif arg in ['--debug', '-d']:
         _DEBUG = True
         logger.setLevel(logging.DEBUG)
+
+    elif arg in ['--config', '-c']:
+        if len(sys.argv) < 1:
+            error('Missing arguments')
+            exit(1)
+        _CONFIG_FILE = sys.argv.pop(0)
+
+    elif arg in ['--bind']:
+        if len(sys.argv) < 1:
+            error('Missing arguments')
+            exit(1)
+        _CONFIG_OVERRIDE_BIND = sys.argv.pop(0)
+        try:
+            ipaddress.IPv4Address(_CONFIG_OVERRIDE_BIND)
+        except:
+            error('Please specify a valid IP address')
+            exit(1)
+
+    elif arg in ['--max-connections']:
+        if len(sys.argv) < 1:
+            error('Missing arguments')
+            exit(1)
+        try:
+            _CONFIG_OVERRIDE_CONNS = int(sys.argv.pop(0))
+            if _CONFIG_OVERRIDE_CONNS < 1:
+                raise Exception()
+        except:
+            error('Maximum number of connections must be positive a number')
+            exit(1)
+
+    elif arg in ['--dont-pull']:
+        _PULL = False
 
     else:
         printHelp()
