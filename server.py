371a64ea001b2c01a629e5225f888feb5195078f
Dominik Dancs <dodancs@moow.info>
2020-10-27 22:13:38 +0100

    Added local socket binding and prepared for real-time honeypot management

--- server.py
+++ server.py
@@ -8,12 +8,14 @@ import logging
 import logging.handlers
 import docker
 import ipaddress
+import socket
+import threading
 
-#####################
-#      Logging      #
-#####################
+########################
+#       Logging        #
+########################
 
-logger = logging.getLogger('canary-server')
+logger = logging.getLogger('hih-docker')
 logging_format = logging.Formatter(
     '%(name)s: [%(levelname)s] %(message)s'
 )
@@ -23,19 +25,21 @@ if os.name != 'nt':
     logger.addHandler(handler_linux)
 logger.setLevel(logging.INFO)
 
-#####################
-#       Helpers     #
-#####################
+########################
+#        Helpers       #
+########################
 
-_VERSION = '1.0.0'
-_DEBUG = False
-_CONFIG_FILE = 'config.json'
-_CONFIG = None
-_CONFIG_OVERRIDE_BIND = None
-_CONFIG_OVERRIDE_CONNS = None
-_DOCKER_CLIENT = None
-_DOCKER_IMAGES = []
-_PULL = True
+_VERSION = '1.0.0'  # app version
+_DEBUG = False  # enable or disable debugging output
+_CONFIG_FILE = 'config.json'  # config file name
+_CONFIG = None  # parsed configuration
+_CONFIG_OVERRIDE_BIND = None  # override bind IP address from config
+_CONFIG_OVERRIDE_CONNS = None  # override maximum number of connections from config
+_DOCKER_CLIENT = None  # Docker client
+_DOCKER_IMAGES = []  # all docker images used
+_PULL = True  # pull or do not pull images
+_SESSIONS = []  # connected sessions
+_SERVERS = []  # running servers
 
 
 def info(message):
@@ -73,9 +77,21 @@ def printVersion():
     cprint('HIH-Docker version {}'.format(_VERSION), 'cyan')
     exit(0)
 
-#####################
-#   Config loader   #
-#####################
+########################
+#      Exceptions      #
+########################
+
+
+class CannotBindPort(Exception):
+    def __init__(self, honeypot):
+        self.honeypot = honeypot
+        self.message = 'Cannot bind local port \'{}\' - already in use!'.format(
+            honeypot['port'])
+        super().__init__(self.message)
+
+########################
+#     Config loader    #
+########################
 
 
 def loadConfig():
@@ -103,6 +119,10 @@ def loadConfig():
 
     debug(_CONFIG)
 
+########################
+#    Initialization    #
+########################
+
 
 def init():
     global _CONFIG, _DOCKER_IMAGES, _DOCKER_CLIENT
@@ -127,10 +147,70 @@ def init():
             info('Pulling {}...'.format(image))
             _DOCKER_CLIENT.images.pull(image)
 
+    # bind honeypot servers
+    for honeypot in _CONFIG['honeypots']:
+        try:
+            server(honeypot)
+        except CannotBindPort as e:
+            error(e.message)
+            exit(1)
+
+########################
+#      TCP server      #
+########################
+
+
+def server(honeypot):
+    # create new TCP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # allow multiple connections on the same address
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # bind IP address and port
+        host = _CONFIG['bind'] if _CONFIG_OVERRIDE_BIND == None else _CONFIG_OVERRIDE_BIND
+        s.bind(host, honeypot['port'])
+        # set maximum number of connections
+        s.listen(_CONFIG['max_connections'] if _CONFIG_OVERRIDE_CONNS ==
+                 None else _CONFIG_OVERRIDE_CONNS)
+        _SERVERS.append(s)
+    except:
+        raise CannotBindPort(honeypot)
+
+    info('Server for \'{}\' honeypot has started on {}.'.format(
+        honeypot['name'], host + ':' + honeypot['port']))
+
+    while True:
+        local_socket, local_address = s.accept()
+        info('New connection from {}:{}.'.format(
+            local_address[0], local_address[1]))
+        launchHoneypot(honeypot, local_socket, local_address)
+
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+
+########################
+#  Honeypot management #
+########################
+
+
+def launchHoneypot(honeypot, local_socket, local_address):
+    # TODO: create honeypot
+    # TODO: figure out networking
+    # TODO: create new container
+    # TODO: bind socker to container
+    # TODO: track container and connection
+    # TODO: remove container on connection close
+
+    # remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # remote_socket.connect((remote_host, remote_port))
+
+    print('hey')
 
 ########################
 #   Argument parsing   #
 ########################
+
 
 sys.argv.pop(0)
 while len(sys.argv) > 0:
