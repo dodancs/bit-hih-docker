1e4f266dabc476575c922ed1b73e4ab50fa6ed96
Dominik Dancs <dodancs@moow.info>
2020-11-04 16:21:15 +0100

    Added options for defining port range for local sockets and additional options container images. Added configuration parsing for launching Docker containers. Added lookup for already downloaded Docker images.

--- server.py
+++ server.py
@@ -37,9 +37,10 @@ _CONFIG_OVERRIDE_BIND = None  # override bind IP address from config
 _CONFIG_OVERRIDE_CONNS = None  # override maximum number of connections from config
 _DOCKER_CLIENT = None  # Docker client
 _DOCKER_IMAGES = []  # all docker images used
-_PULL = True  # pull or do not pull images
+_FORCE_PULL = False  # dorce pull fresh docker images
 _SESSIONS = []  # connected sessions
-_SERVERS = []  # running servers
+_SERVER_SOCKETS = []  # running servers
+_PORTS_USED = []  # used local ports
 
 
 def info(message):
@@ -67,7 +68,7 @@ def printHelp():
        --bind [ADDR]            Override IP address bind\n\
        --max-connections [N]    Override maximum number of connections to the server\n\
     -c --config [FILE]          Use custom config file location\n\
-       --dont-pull              Don\'t pull images from Docker hub\
+       --force-pull             Force pull images from Docker registry\
     ', 'cyan')
     exit(1)
 
@@ -87,6 +88,12 @@ class CannotBindPort(Exception):
         self.honeypot = honeypot
         self.message = 'Cannot bind local port \'{}\' - already in use!'.format(
             honeypot['port'])
+        super().__init__(self.message)
+
+
+class NoPortsAvailable(Exception):
+    def __init__(self):
+        self.message = 'No more ports available! Cannot start honeypot.'.format()
         super().__init__(self.message)
 
 ########################
@@ -117,7 +124,7 @@ def loadConfig():
 
     info('Server configuration loaded successfully!')
 
-    debug(_CONFIG)
+    debug('Loaded config: {}'.format(_CONFIG))
 
 ########################
 #    Initialization    #
@@ -129,7 +136,8 @@ def init():
 
     # initialize Docker connector
     _DOCKER_CLIENT = docker.from_env()
-    debug(_DOCKER_CLIENT.containers.list())
+    debug('List of running docker containers: {}'.format(
+        _DOCKER_CLIENT.containers.list()))
 
     # initialize script
     loadConfig()
@@ -138,12 +146,20 @@ def init():
     for honeypot in _CONFIG['honeypots']:
         if honeypot['image'] not in _DOCKER_IMAGES:
             _DOCKER_IMAGES.append(honeypot['image'])
-    debug(_DOCKER_IMAGES)
+    debug('Docker images parsed from config: {}'.format(_DOCKER_IMAGES))
 
     # pull docker images
-    if _PULL:
-        info('Updating Docker images...')
-        for image in _DOCKER_IMAGES:
+    info('Updating Docker images...')
+    for image in _DOCKER_IMAGES:
+        info('Checking for {}...'.format(image))
+        if not _FORCE_PULL:
+            try:
+                _DOCKER_CLIENT.images.get(image)
+                info('Image {} is available.'.format(image))
+            except:
+                info('Pulling {}...'.format(image))
+                _DOCKER_CLIENT.images.pull(image)
+        else:
             info('Pulling {}...'.format(image))
             _DOCKER_CLIENT.images.pull(image)
 
@@ -169,17 +185,25 @@ def server(honeypot):
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # bind IP address and port
         host = _CONFIG['bind'] if _CONFIG_OVERRIDE_BIND == None else _CONFIG_OVERRIDE_BIND
-        s.bind(host, honeypot['port'])
+        s.bind((host, honeypot['port']))
         # set maximum number of connections
         s.listen(_CONFIG['max_connections'] if _CONFIG_OVERRIDE_CONNS ==
                  None else _CONFIG_OVERRIDE_CONNS)
-        _SERVERS.append(s)
+        _SERVER_SOCKETS.append(s)
     except:
         raise CannotBindPort(honeypot)
 
     info('Server for \'{}\' honeypot has started on {}.'.format(
-        honeypot['name'], host + ':' + honeypot['port']))
+        honeypot['name'], str(host) + ':' + str(honeypot['port'])))
 
+    waitForConnection(honeypot, s)
+    t = threading.Thread(target=waitForConnection, args=(
+        honeypot, s))
+    t.start()
+    print('after thread')
+
+
+def waitForConnection(honeypot, s):
     while True:
         local_socket, local_address = s.accept()
         info('New connection from {}:{}.'.format(
@@ -194,8 +218,78 @@ def server(honeypot):
 ########################
 
 
+def getPort():
+    global _CONFIG, _PORTS_USED
+    available = [*range(_CONFIG['ports_range'][0],
+                        _CONFIG['ports_range'][1] + 1, 1)]
+    available = available - _PORTS_USED
+    if (len(available) > 0):
+        return available[0]
+    else:
+        raise NoPortsAvailable()
+
+
 def launchHoneypot(honeypot, local_socket, local_address):
+    global _DOCKER_CLIENT
+
+    try:
+        h_command = command = honeypot['options']['command']
+    except:
+        h_command = None
+
+    try:
+        h_environment = command = honeypot['options']['environment']
+    except:
+        h_environment = []
+
+    try:
+        h_network = command = honeypot['options']['network']
+    except:
+        h_network = None
+
+    try:
+        h_network_mode = command = honeypot['options']['network_mode']
+    except:
+        h_network_mode = None
+
+    h_ports = {}
+    h_ports[honeypot['container_port']] = getPort()
+    print(h_ports)
+    try:
+        h_ports.update(command=honeypot['options']['ports'])
+    except:
+        h_ports = None
+
+    try:
+        h_read_only = command = honeypot['options']['read_only']
+    except:
+        h_read_only = False
+
+    try:
+        h_user = command = honeypot['options']['user']
+    except:
+        h_user = None
+
+    try:
+        h_volumes = command = honeypot['options']['volumes']
+    except:
+        h_volumes = {}
+
     # TODO: create honeypot
+    h = _DOCKER_CLIENT.containers.run(
+        image=honeypot['image'],
+        command=h_command,
+        auto_remove=True,
+        detach=True,
+        environment=h_environment,
+        network=h_network,
+        network_mode=h_network_mode,
+        ports=h_ports,
+        read_only=h_read_only,
+        restart_policy='on-failure',
+        user=h_user,
+        volumes=h_volumes)
+
     # TODO: figure out networking
     # TODO: create new container
     # TODO: bind socker to container
@@ -256,8 +350,8 @@ while len(sys.argv) > 0:
             error('Maximum number of connections must be positive a number')
             exit(1)
 
-    elif arg in ['--dont-pull']:
-        _PULL = False
+    elif arg in ['--force-pull']:
+        _FORCE_PULL = True
 
     else:
         printHelp()
