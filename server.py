0d4e3dfe6ad26f1467ff50b70a3bf3296b40b857
Dominik Dancs <dodancs@moow.info>
2020-11-05 11:18:27 +0100

    Added socket data transfer, added option to kill the server and cleanup

--- server.py
+++ server.py
@@ -2,14 +2,16 @@ import json
 from jsonschema import validate
 import os
 import sys
+import signal
 from termcolor import colored, cprint
-import uuid
 import logging
 import logging.handlers
 import docker
 import ipaddress
 import socket
 import threading
+import time
+import itertools
 
 ########################
 #       Logging        #
@@ -35,12 +37,16 @@ _CONFIG_FILE = 'config.json'  # config file name
 _CONFIG = None  # parsed configuration
 _CONFIG_OVERRIDE_BIND = None  # override bind IP address from config
 _CONFIG_OVERRIDE_CONNS = None  # override maximum number of connections from config
+_HOST = None  # bind host
 _DOCKER_CLIENT = None  # Docker client
 _DOCKER_IMAGES = []  # all docker images used
 _FORCE_PULL = False  # dorce pull fresh docker images
-_SESSIONS = []  # connected sessions
+_SESSIONS = {}  # connected sessions
 _SERVER_SOCKETS = []  # running servers
+_SOCKETS = []  # client sockets
 _PORTS_USED = []  # used local ports
+_PORTS_LOCK = None  # lock for multithreaded access
+_THREADS = []  # all threads
 
 
 def info(message):
@@ -77,6 +83,32 @@ def printVersion():
     global _VERSION
     cprint('HIH-Docker version {}'.format(_VERSION), 'cyan')
     exit(0)
+
+
+def Diff(li1, li2):
+    return (list(list(set(li1) - set(li2)) + list(set(li2) - set(li1))))
+
+
+def stopAll(signal, frame):
+    global _SERVER_SOCKETS, _SESSIONS, _DOCKER_CLIENT, _THREADS
+    info('Stopping all services...')
+    for i in _SESSIONS.keys():
+        try:
+            h = _DOCKER_CLIENT.containers.get(i)
+            h.stop()
+        except:
+            pass
+    for s in itertools.chain(_SERVER_SOCKETS, _SOCKETS):
+        try:
+            s.shutdown(socket.SHUT_RDWR)
+        except:
+            pass
+        s.close()
+    # for t in _THREADS:
+    #     t.join()
+
+    sys.exit(0)
+
 
 ########################
 #      Exceptions      #
@@ -132,7 +164,12 @@ def loadConfig():
 
 
 def init():
-    global _CONFIG, _DOCKER_IMAGES, _DOCKER_CLIENT
+    global _CONFIG, _DOCKER_IMAGES, _DOCKER_CLIENT, _HOST, _FORCE_PULL, _PORTS_LOCK
+
+    signal.signal(signal.SIGTERM, stopAll)
+    signal.signal(signal.SIGINT, stopAll)
+
+    _PORTS_LOCK = threading.Lock()
 
     # initialize Docker connector
     _DOCKER_CLIENT = docker.from_env()
@@ -141,6 +178,8 @@ def init():
 
     # initialize script
     loadConfig()
+
+    _HOST = _CONFIG['bind']
 
     # scan for used images
     for honeypot in _CONFIG['honeypots']:
@@ -167,9 +206,12 @@ def init():
     for honeypot in _CONFIG['honeypots']:
         try:
             server(honeypot)
-        except CannotBindPort as e:
+        except Exception as e:
             error(e.message)
             exit(1)
+
+    while True:
+        time.sleep(1)
 
 ########################
 #      TCP server      #
@@ -177,6 +219,8 @@ def init():
 
 
 def server(honeypot):
+    global _HOST, _CONFIG_OVERRIDE_CONNS, _SERVER_SOCKETS, _THREADS
+
     # create new TCP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -184,8 +228,7 @@ def server(honeypot):
         # allow multiple connections on the same address
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # bind IP address and port
-        host = _CONFIG['bind'] if _CONFIG_OVERRIDE_BIND == None else _CONFIG_OVERRIDE_BIND
-        s.bind((host, honeypot['port']))
+        s.bind((_HOST, honeypot['port']))
         # set maximum number of connections
         s.listen(_CONFIG['max_connections'] if _CONFIG_OVERRIDE_CONNS ==
                  None else _CONFIG_OVERRIDE_CONNS)
@@ -194,24 +237,64 @@ def server(honeypot):
         raise CannotBindPort(honeypot)
 
     info('Server for \'{}\' honeypot has started on {}.'.format(
-        honeypot['name'], str(host) + ':' + str(honeypot['port'])))
+        honeypot['name'], str(_HOST) + ':' + str(honeypot['port'])))
 
-    waitForConnection(honeypot, s)
     t = threading.Thread(target=waitForConnection, args=(
         honeypot, s))
+    t.daemon = True
     t.start()
-    print('after thread')
+    _THREADS.append(t)
 
 
 def waitForConnection(honeypot, s):
+    global _SOCKETS
+
     while True:
         local_socket, local_address = s.accept()
         info('New connection from {}:{}.'.format(
             local_address[0], local_address[1]))
+        _SOCKETS.append(local_socket)
         launchHoneypot(honeypot, local_socket, local_address)
 
     s.shutdown(socket.SHUT_RDWR)
     s.close()
+
+
+def dataTransfer(honeypot, h_id, local_address, src, dst, direction):
+    global _DOCKER_CLIENT, _PORTS_USED, _SESSIONS
+
+    try:
+        src_name = src.getsockname()
+        src_address = src_name[0]
+        src_port = src_name[1]
+        dst_name = dst.getsockname()
+        dst_address = dst_name[0]
+        dst_port = dst_name[1]
+        while True:
+            buffer = src.recv(0x400)
+            if len(buffer) == 0:
+                break
+            dst.send(dataHandler(buffer))
+        src.shutdown(socket.SHUT_RDWR)
+        src.close()
+        dst.shutdown(socket.SHUT_RDWR)
+        dst.close()
+    except:
+        pass
+
+    try:
+        h = _DOCKER_CLIENT.containers.get(h_id)
+        _PORTS_USED.remove(_SESSIONS[h_id])
+        h.stop()
+        debug('Stopped \'{}\' container for host: {} - {}.'.format(
+            honeypot['name'], local_address[0], h_id))
+    except:
+        pass
+
+
+def dataHandler(buffer):
+    return buffer
+
 
 ########################
 #  Honeypot management #
@@ -219,18 +302,23 @@ def waitForConnection(honeypot, s):
 
 
 def getPort():
-    global _CONFIG, _PORTS_USED
+    global _CONFIG, _PORTS_USED, _PORTS_LOCK
+    _PORTS_LOCK.acquire()
     available = [*range(_CONFIG['ports_range'][0],
                         _CONFIG['ports_range'][1] + 1, 1)]
-    available = available - _PORTS_USED
+    available = Diff(available, _PORTS_USED)
+
     if (len(available) > 0):
+        _PORTS_USED.append(available[0])
+        _PORTS_LOCK.release()
         return available[0]
     else:
+        _PORTS_LOCK.release()
         raise NoPortsAvailable()
 
 
 def launchHoneypot(honeypot, local_socket, local_address):
-    global _DOCKER_CLIENT
+    global _DOCKER_CLIENT, _SESSIONS, _CONFIG_OVERRIDE_BIND, _THREADS
 
     try:
         h_command = command = honeypot['options']['command']
@@ -253,12 +341,11 @@ def launchHoneypot(honeypot, local_socket, local_address):
         h_network_mode = None
 
     h_ports = {}
-    h_ports[honeypot['container_port']] = getPort()
-    print(h_ports)
+    h_ports[honeypot['container_port']] = (_HOST, getPort())
     try:
         h_ports.update(command=honeypot['options']['ports'])
     except:
-        h_ports = None
+        pass
 
     try:
         h_read_only = command = honeypot['options']['read_only']
@@ -275,7 +362,6 @@ def launchHoneypot(honeypot, local_socket, local_address):
     except:
         h_volumes = {}
 
-    # TODO: create honeypot
     h = _DOCKER_CLIENT.containers.run(
         image=honeypot['image'],
         command=h_command,
@@ -286,25 +372,42 @@ def launchHoneypot(honeypot, local_socket, local_address):
         network_mode=h_network_mode,
         ports=h_ports,
         read_only=h_read_only,
-        restart_policy='on-failure',
         user=h_user,
         volumes=h_volumes)
 
-    # TODO: figure out networking
-    # TODO: create new container
-    # TODO: bind socker to container
-    # TODO: track container and connection
-    # TODO: remove container on connection close
+    hh = _DOCKER_CLIENT.containers.get(h.id)
 
-    # remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # remote_socket.connect((remote_host, remote_port))
+    # print(hh.attrs['NetworkSettings'])
+    # h_ip = hh.attrs['NetworkSettings']['IPAddress']
+    h_port = h_ports[honeypot['container_port']][1]
 
-    print('hey')
+    # print('Docker IP: {}:{}'.format(h_ip, h_port))
+
+    debug('Started \'{}\' container on port {} for host: {} - {}.'.format(
+        honeypot['name'], h_port, local_address[0], h.id))
+
+    _SESSIONS[h.id] = h_port
+    print(_SESSIONS)
+
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect(('127.0.0.1', h_port))
+    _SOCKETS.append(remote_socket)
+
+    s = threading.Thread(target=dataTransfer, args=(
+        honeypot, h.id, local_address, remote_socket, local_socket, False))
+    r = threading.Thread(target=dataTransfer, args=(
+        honeypot, h.id, local_address, local_socket, remote_socket, True))
+    s.daemon = True
+    r.daemon = True
+    s.start()
+    r.start()
+    _THREADS.append(s)
+    _THREADS.append(r)
+
 
 ########################
 #   Argument parsing   #
 ########################
-
 
 sys.argv.pop(0)
 while len(sys.argv) > 0:
@@ -331,9 +434,11 @@ while len(sys.argv) > 0:
         if len(sys.argv) < 1:
             error('Missing arguments')
             exit(1)
+
         _CONFIG_OVERRIDE_BIND = sys.argv.pop(0)
         try:
             ipaddress.IPv4Address(_CONFIG_OVERRIDE_BIND)
+            _HOST = _CONFIG_OVERRIDE_BIND
         except:
             error('Please specify a valid IP address')
             exit(1)
